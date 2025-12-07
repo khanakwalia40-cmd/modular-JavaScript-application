fbd89e154625ce58e4c71f0eaa94b5c324ae5cca
khanakwalia40-cmd <khanakwalia40@gmail.com>
2025-12-07 14:54:06 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,8 @@
+// footer.js
+export function footer() {
+  return `
+    <footer style="margin-top:40px; padding:10px; background:#eee; text-align:center;">
+      <p>Todos App &copy; 2025</p>
+    </footer>
+  `;
+}
