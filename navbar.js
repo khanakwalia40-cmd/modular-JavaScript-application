09f5df9f4275d14512fa64e5d452f0f4cd303468
khanakwalia40-cmd <khanakwalia40@gmail.com>
2025-12-07 14:53:37 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,11 @@
+// navbar.js
+export function navbar() {
+  return `
+    <nav style="display:flex; gap:16px; padding:10px; background:#eee;">
+      <a href="./index.html">Home</a>
+      <a href="./signup.html">Signup</a>
+      <a href="./login.html">Login</a>
+      <a href="./todos.html">Todos</a>
+    </nav>
+  `;
+}
